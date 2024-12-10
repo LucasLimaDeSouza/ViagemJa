de2f8b91a46a55f7ef15ab0b432fa528ae2f7937
LucasLimaDeSouza <lukxx23121996@gmail.com>
2024-12-10 19:07:40 -0300

    feat(screen Welcome)

--- app/src/main/java/com/example/viagemja/ui/theme/Color.kt
+++ app/src/main/java/com/example/viagemja/ui/theme/Color.kt
@@ -2,6 +2,12 @@ package com.example.viagemja.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val GreenV = Color(0xFF3BCA79)
+val BlueV = Color(0xFF1E2044)
+val WhiteV = Color(0xFFD9D9D9)
+val GreyV = Color(0xFFB5B5B5)
+
+
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
--- app/src/main/java/com/example/viagemja/ui/theme/Theme.kt
+++ app/src/main/java/com/example/viagemja/ui/theme/Theme.kt
@@ -11,16 +11,17 @@ import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalContext
 
+
 private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
+    primary = WhiteV,
+    secondary = GreyV,
+    tertiary = BlueV
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = WhiteV,
+    secondary = GreyV,
+    tertiary = BlueV
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
